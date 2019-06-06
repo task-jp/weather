0d80b21298c8b66952805021113ace192a972821
Tasuku Suzuki <tasuku.suzuki@qbc.io>
2019-06-06 14:43:52 +0900

    Set Application information for settings

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@ int main(int argc, char *argv[])
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
     QGuiApplication app(argc, argv);
+    app.setApplicationDisplayName(QStringLiteral("Weather"));
+    app.setOrganizationName(QStringLiteral("TasukuSuzuki"));
+    app.setOrganizationDomain(QStringLiteral("qt5.jp"));
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
