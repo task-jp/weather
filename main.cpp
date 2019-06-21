4d9683f7cbfe9be16b837432d501a3b9f32d59ce
Tasuku Suzuki <stasuku@gmail.com>
2019-06-21 12:59:27 +0900

    Add -close option

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
+#include <QtCore/QDebug>
 #include <QtGui/QGuiApplication>
 #include <QtGui/QFontDatabase>
 #include <QtQml/QQmlApplicationEngine>
+#include <QtQuick/QQuickWindow>
+
+#ifdef Q_OS_LINUX
+#include <sys/time.h>
+#include <sys/resource.h>
+#endif
 
 int main(int argc, char *argv[])
 {
@@ -17,6 +24,20 @@ int main(int argc, char *argv[])
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
     if (engine.rootObjects().isEmpty())
         return -1;
+    if (argc > 1 && QString("-close") == argv[1]) {
+        QQuickWindow *window = qobject_cast<QQuickWindow *>(engine.rootObjects().first());
+        if (window) {
+            QObject::connect(window, &QQuickWindow::frameSwapped, [&app]() {
+#ifdef Q_OS_LINUX
+                rusage usage;
+                if (getrusage(RUSAGE_SELF, &usage) == 0) {
+                    qInfo() << usage.ru_stime.tv_sec << usage.ru_stime.tv_usec << usage.ru_utime.tv_sec << usage.ru_utime.tv_usec << usage.ru_maxrss;
+                }
+#endif
+                QMetaObject::invokeMethod(&app, "quit", Qt::QueuedConnection);
+            });
+        }
+    }
 
     return app.exec();
 }
